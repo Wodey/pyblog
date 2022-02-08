c02ec4268564ce4c718cc6d09a74953ab221d4a5
Wodey <54207104+Wodey@users.noreply.github.com>
2022-02-09 01:50:54 +0300

    + create an update article function + delete an article function + create user function + get user function + promote user function + delete user function

--- db.py
+++ db.py
@@ -35,6 +35,7 @@ class Db:
         query = f'SELECT * FROM ARTICLES WHERE id = {id}'
         cursor.execute(query)
 
+        result = None
         for i in cursor:
             result = i
 
@@ -47,7 +48,7 @@ class Db:
         cursor = self.connection.cursor()
         cursor.execute('USE PYBLOG')
 
-        start = (page-1) * 20
+        start = (page - 1) * 20
 
         query = f'SELECT id, title, description, date, author_id FROM Articles LIMIT {start}, 20'
 
@@ -61,11 +62,121 @@ class Db:
         cursor.close()
         return result
 
+    # 0 if it succeeds, 1 if it fails
+    def update_article(self, id: int, data: {}) -> int:
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute('USE PYBlOG')
+
+                query = f'UPDATE Articles SET '
+                for i in data:
+                    query += f'{i} = "{data[i]}", '
+                query = query[:-2]
+                query += f'WHERE id = {id}'
+
+                cursor.execute(query)
+
+                return 0
+        except Error as e:
+            print(e)
+            return 1
+
+    # 1 if it fails and 0 if it succeeds
+    def delete_article(self, id: int) -> int:
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute('USE PYBLOG')
+
+                query = 'DELETE FROM ARTICLES WHERE id = ?'
+
+                cursor.execute(query, (id,))
+
+                self.connection.commit()
+                return 0
+
+        except Error as e:
+            print(e)
+            return 1
+
+    # 1 if it fails and 0 if it succeeds
+    def make_an_user(self, data: {}) -> int:
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute('USE PYBLOG')
+
+                query = 'INSERT INTO Users(username, password, role) ' \
+                        'VALUES (%s, %s, %s)'
+
+                cursor.execute(query, (data['username'], data['password'], data['role']))
+
+                return 0
+
+        except Error as err:
+            print(err)
+            return 1
+
+    # 1 if it fails and a user if it succeeds
+    def get_an_user(self, username: str) -> ():
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute('USE PYBLOG')
+
+                query = 'SELECT * FROM Users WHERE id = %s'
+
+                cursor.execute(query, (id,))
+
+                result = None
+                for i in cursor:
+                    result = i
+
+                return result
+        except Error as err:
+            print(err)
+            return 1
+
+    # 1 if it fails and 0 if it succeeds
+    def promote_user(self, id: int, new_role: int) -> int:
+        try:
+            with self.connection.cursor() as cursor:
+                cursor.execute('USE PYBLOG')
+
+                query = 'UPDATE Users' \
+                        'SET role = %s' \
+                        'WHERE id = %s'
+
+                cursor.execute(query, (new_role, id))
+
+                return 0
+
+        except Error as err:
+            print(err)
+            return 1
+
+        # 1 if it fails and 0 if it succeeds
+        def delete_user(self, id: int) -> int:
+            try:
+                with self.connection.cursor() as cursor:
+                    cursor.execute('USE PYBLOG')
+
+                    query = 'DELETE FROM USERS WHERE id = ?'
+
+                    cursor.execute(query, (id,))
+
+                    self.connection.commit()
+                    return 0
+
+            except Error as e:
+                print(e)
+                return 1
+
     def __delete__(self):
         self.connection.close()
 
 
 if __name__ == "__main__":
     mydb = Db()
-    print(mydb.get_article(1))
     print(mydb.get_articles(1))
+    print(mydb.delete_article(3))
+    print(mydb.get_article(3))
+    print(mydb.get_articles(1))
+    # TEST NEW USER FUNCTIONS TOMMOROW
